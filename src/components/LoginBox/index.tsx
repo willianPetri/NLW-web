1077a455362ed87c2b698e70c0942a3c6098af8c
willian <wopetri11@gmail.com>
2021-10-20 12:24:41 -0400

    added api

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -2,10 +2,13 @@ import { VscGithubInverted } from 'react-icons/vsc'
 import styles from './styles.module.scss';
 
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=f0a7a18cc5a720c152cb`;
+
+
   return(
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24"/>
         Entrar com Github
       </a>
