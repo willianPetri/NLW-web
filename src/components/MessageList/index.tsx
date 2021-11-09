2bd63842c1501b8a4ca318c89ded4904d9601048
willian <wopetri11@gmail.com>
2021-11-09 12:14:56 -0400

    mudei o link da api

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -16,7 +16,7 @@ type Message = {
 
 const messagesQueue: Message[] = [];
 
-const socket = io('http://localhost:4000');
+const socket = io('https://dowhile2021.herokuapp.com');
 
 socket.on('new_message', (newMessage: Message) => {
   messagesQueue.push(newMessage);
